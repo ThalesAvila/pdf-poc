00f5736a9a868c91bb30bd3e387d79bbd01c86f8
Thales <thales@carteiraglobal.com>
2019-09-06 17:03:25 -0300

    Front-end form

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import "./App.css";
+
+class App extends React.Component {
+  state = { name: "", receiptId: 0, value1: 0, value2: 0 };
+
+  handleChange = ({ target: { value, name } }) =>
+    this.setState({ [name]: value });
+
+  createDownloadPdf = () => {
+    axios.post("/create-pdf", this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Receipt ID"
+          name="receiptId"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price 1"
+          name="price1"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price 2"
+          name="price2"
+          onChange={this.handleChange}
+        />
+        <button onClick={this.createDownloadPdf}>Download PDF</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
